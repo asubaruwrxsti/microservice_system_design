a16ad1da84cbec20aaa788f2f5f955fc81493c10
Arlind <arlind12344@gmail.com>
2023-11-09 11:56:41 +0100

    refactoring

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -2,7 +2,7 @@ import os, gridfs, pika, json
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from auth import validate
-from auth_svc import access
+from auth_service import access
 from storage import util
 
 server = Flask(__name__)
